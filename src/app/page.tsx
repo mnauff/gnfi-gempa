c272be2d5397ba49766f1f183ae6a3bc96019d35
Naufal Rafianto <mnaufalrafianto@gmail.com>
2024-06-26 07:37:14 +0300

    feat: initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,186 @@
-import Image from "next/image";
+/* eslint-disable jsx-a11y/alt-text */
+import {
+  COMPONENT_PARAGRAPH,
+  COMPONENT_SECTION_HEADER,
+} from "@/data/component-front";
+import { Image } from "@/stories/Image";
+import { Paragraph } from "@/stories/Paragraph";
+import SectionGroup from "@/stories/SectionGroup";
+import { SectionHeader } from "@/stories/SectionHeader";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="min-h-screen w-screen relative">
+      <SectionHeader
+        block={{
+          component: COMPONENT_SECTION_HEADER,
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          componentProps: {
+            coverImage: "/assets/cover.png",
+            title: "Ada Gempa, Kita Harus Ngapain?",
+          },
+        }}
+      />
+      <SectionGroup
+        className="bg-[#F1F0ED] mx-auto min-h-screen px-10 md:px-40 xl:px-[30rem]"
+        sectionId="section--4"
+      >
+        <Paragraph
+          block={{
+            component: COMPONENT_PARAGRAPH,
+            componentProps: {
+              className: "py-8 text-black",
+              children: (
+                <>
+                  <p>
+                    Tiga prinsip dasar itu perlu dilakukan, namun tidak boleh
+                    secara sembarangan. Ada beberapa hal yang perlu dicermati
+                    agar drop, cover, dan hold bisa menghasilkan perlindungan
+                    secara maksimal. Benda-benda yang bisa jatuh menimpa badan,
+                    seperti jendela kaca, rak, lemari, maupun barang-barang yang
+                    tergantung, yakni lukisan, cermin, jam dinding, dan lampu
+                    gantung wajib dihindari. Segera lindungi kepala menggunakan
+                    kedua tangan atau benda di sekitar. “Jika di dalam ruangan
+                    serba kaca, berlindung di bawah meja. Kalau tidak ada meja,
+                    cari hal lain yang bisa melindungi kepala kita dari
+                    kejatuhan material,” kata Abah Lala. Posisi tubuh dan lokasi
+                    di mana Kawan berada juga patut diperhatikan. Apabila Kawan
+                    sedang dalam keadaan tidur atau berbaring, tutuplah kepala
+                    menggunakan bantal untuk melindungi kepala dari benda-benda
+                    yang jatuh.
+                  </p>
+                </>
+              ),
+            },
+          }}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <Image
+          block={{
+            component: 2,
+            componentProps: {
+              imageAlt: "Membangun IKN, Mewujudkan Wacana Lama Indonesia",
+              imageUrl: "/assets/1.png",
+            },
+          }}
+        />
+        <Paragraph
+          block={{
+            component: COMPONENT_PARAGRAPH,
+            componentProps: {
+              className: "py-8 text-black",
+              children: (
+                <>
+                  <p>
+                    Jika Kawan berada di lantai satu bangunan, segeralah keluar
+                    dan menuju tempat terbuka apabila memungkinkan, tentu sambil
+                    tetap lindungi kepala dan leher mengunakan kedua tangan atau
+                    benda lainnya. Keluar dari bangunan dengan segera bisa jadi
+                    sulit apabila seseorang sedang berada di lantai dua atau
+                    lebih tinggi. Jika menghadapi situasi ini, berlindunglah di
+                    bawah meja yang kokoh sambil memegang kaki meja agar tidak
+                    bergeser, lalu lekas merapat ke dinding bagian pojok dengan
+                    merunduk seraya melindungi kepala. Dinding dekat elevator
+                    juga cocok untuk jadi tempat berlindung karena bagian itu
+                    adalah konstruksi terkuat sebuah gedung. Setelah gempa reda,
+                    barulah evakuasi diri ke titik kumpul yang aman guna
+                    mengantisipasi gempa susulan. “Selama gempa harus segera
+                    mepet ke dinding. Kalau di tengah khawatir tertimpa dari
+                    atas,” imbuh Abah Lala. Saat terjadi gempa, biasanya
+                    pengelola gedung melarang penggunaan elevator. Namun jika
+                    Kawan sedang berada di dalamnya, yang perlu dilakukan adalah
+                    tekan tombol semua lantai, dan segeralah keluar saat pintu
+                    terbuka di lantai berapa pun. Jika pintu tak terbuka, tekan
+                    tombol darurat untuk memanggil bantuan. Kemungkinan terburuk
+                    yang bisa saja terjadi adalah terjebak dalam ruangan atau
+                    tertimpa benda sehingga tidak dapat bergerak. Jika hal itu
+                    terjadi, jangan habiskan energi dengan terus-menerus
+                    berteriak. Lebih baik ketuk benda yang ada untuk memberi
+                    tanda agar segera mendapatkan pertolongan.
+                  </p>
+                </>
+              ),
+            },
+          }}
+        />
+        <Image
+          block={{
+            component: 2,
+            componentProps: {
+              imageAlt: "Membangun IKN, Mewujudkan Wacana Lama Indonesia",
+              imageUrl: "/assets/2.png",
+            },
+          }}
+        />
+        <Paragraph
+          block={{
+            component: COMPONENT_PARAGRAPH,
+            componentProps: {
+              className: "py-8 text-black",
+              children: (
+                <>
+                  <p>
+                    Mitigasi bencana bisa dilakukan dalam berbagai macam cara.
+                    Mengacu kepada UU Nomor 24 Tahun 2007 tentang Penanggulangan
+                    Bencana, langkah mitigasi dapat berwujud peningkatan
+                    kemampuan menghadapi ancaman bencana maupun pembangunan
+                    fisik. Bagi masyarakat Indonesia yang sehari-harinya hidup
+                    di bawah bayang-bayang gempa, memahami apa yang harus
+                    dilakukan saat gempa terjadi sudah tentu jadi keharusan.
+                    Tujuan utamanya, sudah tentu menyelamatkan diri secepat
+                    mungkin. Untuk mengetahui apa saja yang perlu dilakukan guna
+                    menyelamatkan diri saat terjadi gempa, utamanya yang
+                    berkekuatan besar dan berlangsung lama, GNFI berkesempatan
+                    berbincang dengan Anis Faisal Reza atau yang biasa dikenal
+                    sebagai Abah Lala, Ketua Komunitas Gugus Mitigasi Lebak
+                    Selatan (GMLS).Menurut Abah Lala, jika getaran gempa cukup
+                    besar, masyarakat harus mencari perlindungan di titik
+                    terdekat, misalnya di kolong meja kayu yang cukup kuat atau
+                    sekedar menutup kepala dengan tangan. “Saat gempa itu adalah
+                    posisi kritis. Apalagi saat kita di tempat yang tinggi, kita
+                    tidak akan bisa menempuh perjalanan ke basement dalam waktu
+                    yang singkat. (Saat terjadi) Gempa besar dan berayun, lama
+                    berlari malah akan menambah resiko yang baru, bisa jatuh,”
+                    ujar Abah Lala. Langkah tersebut menjadi sangat penting,
+                    apalagi saat Kawan GNFI tengah berada di lokasi yang tinggi
+                    seperti lantai atas gedung. Mencari perlindungan terdekat
+                    lebih baik baik ketimbang berusaha lari dan turun ke lantai
+                    dasar karena tindakan tersebut justru berbahaya. Abah Lala
+                    menyebut ada tiga prinsip utama untuk menyelamatkan diri
+                    saat terjadi gempa, yakni drop, cover, dan hold. Drop
+                    berarti mengubah posisi dengan menunduk, berjongkok, atau
+                    sujud. Posisi ini dapat meminimalisir Kawan merasakan pusing
+                    dan limbung saat terjadi getaran. Cover artinya menutupi dan
+                    melindungi kepala dengan tangan, tas atau benda lain.
+                    Kemudian, langkah ketiga ialah hold yang artinya berpegangan
+                    erat agar tidak limbung.
+                  </p>
+                </>
+              ),
+            },
+          }}
+        />
+        <Image
+          block={{
+            component: 2,
+            componentProps: {
+              imageAlt: "Membangun IKN, Mewujudkan Wacana Lama Indonesia",
+              imageUrl: "/assets/3.png",
+            },
+          }}
+        />
+      </SectionGroup>
+      {/* <SectionGroup sectionId="4">
+        
+        <Image
+          block={{
+            component: 2,
+            componentProps: {
+              imageAlt: "Membangun IKN, Mewujudkan Wacana Lama Indonesia",
+              imageUrl: "/assets/cover.png",
+            },
+          }}
+        />{" "}
+      </SectionGroup> */}
     </main>
   );
 }
---
+++ src/constants/meta.ts
@@ -0,0 +1,10 @@
+import { isProd } from "./env";
+import meta from "../../public/article/generated-meta.json";
+import metaConfig from "../../meta-config.json";
+const baseUrlWithSlug = isProd
+  ? `${metaConfig.baseUrl}/${meta.slug}`
+  : "http://localhost:3000";
+export const baseUrl = baseUrlWithSlug.replaceAll(
+  !metaConfig.withSlug ? `/${meta.slug}` : "",
+  ""
+);
